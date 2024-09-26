16a9bf2689125962f61b5edbf0455a819aa4d076
Wojtek987 <maciej.woj05@gmail.com>
2024-09-27 01:52:12 +0200

    basic UI for TINY

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    bool is_running = true;
+    string user_command;
+
+    while(is_running) {
+        cout << "\033[1m\033[34m" << "tiny" << "\033[0m" << " < ";
+        cin >> user_command;
+        cout << "\033[1m\033[34m" << "tiny" << "\033[0m" << " > " << "\033[35m" << "undefined: " << user_command << "\033[0m" << endl;
+        if(user_command == "exit")
+            is_running = false;
+    }
+
     return 0;
 }
